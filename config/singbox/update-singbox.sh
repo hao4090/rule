1d62dfb3d9c224f0455cb78a36999d0f77a9a011
hao4090 <171579491@qq.com>
2024-11-20 16:26:22 +0800

    Update update-singbox.sh

--- config/singbox/update-singbox.sh
+++ config/singbox/update-singbox.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-curl -L "http://192.168.31.101:5000/config/https://bayoeorescentpossessicoanseparateuneforescenphocommitte.adoptangelaboradvacotionclwonthorughconfrmcompimentdeseertaltar.org/link/0DxlyeHu9sKQW44c?clash=2&file=https://raw.githubusercontent.com/hao4090/rule/refs/heads/master/config/singbox/config.json" -o /etc/sing-box/config.json
+curl -L "http://192.168.31.101:5000/config/订阅&file=https://raw.githubusercontent.com/hao4090/rule/refs/heads/master/config/singbox/config.json" -o /etc/sing-box/config.json
 killall sing-box || echo "No sing-box process found."
 sleep 3
 sing-box run -c /etc/sing-box/config.json &
